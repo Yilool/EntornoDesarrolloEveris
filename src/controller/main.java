72cc6dbdf687bb2407c2037259f1803adab04f34
yxiangch <yxiangch@SVQ-53S3L12.usersad.everis.int>
2020-04-23 13:12:11 +0200

    "Segundo push JavaProject"

--- src/controller/main.java
+++ src/controller/main.java
@@ -14,6 +14,8 @@ public class main {
 		coCars.setNumPasajero(2);
 		coCars.setTipCombustible("Diesel");
 		
+		System.out.println("Coches:");
+		System.out.println();
 		System.out.println(coche.toString()+"\n");
 		System.out.println(coCars.toString());
 	}
