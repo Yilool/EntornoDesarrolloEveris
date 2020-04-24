9671e7728139ce32b087258f01136d6366e88e16
Yi Xiang Chen <yxiangch@everis.com>
2020-04-24 13:21:09 +0200

    Commit2404: Comit final Prueba Final

--- src/controller/main.java
+++ src/controller/main.java
@@ -1,13 +1,17 @@
 package controller;
 
+import logic.helper;
 import model.Cars;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		Cars coche = new Cars("Marca", "Modelo", 4, "Gasolina");
 		Cars coCars = new Cars();
+
+		System.out.println(helper.Bienvenido());
 		
 		coCars.setMarca("Marca");
 		coCars.setModelo("Modelo");
@@ -17,6 +21,8 @@ public class main {
 		System.out.println("Coches:");
 		System.out.println(coche.toString()+"\n");
 		System.out.println(coCars.toString());
+		System.out.println(helper.numPasajeroString(coche));
+		System.out.println(helper.numPasajeroString(coCars));
 	}
 
 }
--- src/logic/helper.java
+++ src/logic/helper.java
@@ -10,4 +10,20 @@ public class helper {
 	public static void subPasajero(Cars coche, int numPas) {
 		coche.setNumPasajero(coche.getNumPasajero() - numPas);
 	}
+
+	public static String numPasajeroString(Cars coche) {
+		int pasajero = 0;
+
+		pasajero = coche.getNumPasajero();
+
+		return "Hay "+pasajero+" pasajeros";
+	}
+
+	public static String Bienvenido() {
+		StringBuilder aux = new StringBuilder("");
+
+		aux.append("Bienvenido");
+
+		return aux.toString();
+	}
 }
